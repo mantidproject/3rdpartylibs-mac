4c6014aa08ae2ec770e92b1f9d1705b99bcb66a6
Russell Taylor <taylorrj@ornl.gov>
2013-11-11 17:48:36 -0500

    Re #8426. Add scipy version 0.13.0
    This was built via easy_install on a Mountain Lion machine onto which I had
    installed gfortran 4.2.1. It is build against Apple Python 2.7.2 and numpy 1.6.1
    (both of which come pre-installed on OS X).

---
+++ Python/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.13.0'
+version = '0.13.0'
+full_version = '0.13.0'
+git_revision = '1cc8beed5362ed290f5a8ddf4e99db49b4de6286'
+release = True
+
+if not release:
+    version = full_version
